154a42da7852148a6fdded5b633159caf0be9215
Devin Warrick <dev.warr@gmail.com>
2020-02-25 20:58:13 -0600

    Removes extra console

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import log from "./helpers/log";
 
 // PORT we will listen on
 const PORT = process.env.PORT || 2019;
-console.log(PORT)
-console.log("here")
+
 // Create serverInstance so we can connect via webSocket
 const serverInstance = server.listen(PORT, () =>
     /* eslint-disable */
